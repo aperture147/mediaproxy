7610214c0943aa2c34c9ef619d353d4b56f3ded1
HaiVQ <aperture147@gmail.com>
2021-01-28 14:58:01 +0700

    Add file system storage

--- storage/fs.go
+++ storage/fs.go
@@ -1,1 +1,27 @@
 package storage
+
+import (
+	"io/ioutil"
+	"path"
+)
+
+type FileSystemStorage struct {
+	Path string
+}
+
+func NewFileSystemStorage(path string) Storage {
+	return FileSystemStorage{
+		Path: path,
+	}
+}
+
+/*
+fileName here should be a full path with extension of the file
+this function returns full path of the uploaded file
+saving file to
+*/
+func (s FileSystemStorage) Save(fileName, _ string, buf *[]byte) (string, error) {
+	fullPath := path.Join(s.Path, fileName)
+	err := ioutil.WriteFile(fullPath, *buf, 0755)
+	return fullPath, err
+}
